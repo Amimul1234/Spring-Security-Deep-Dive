c16e69146f2fd6a3066273adde87cf2c4586e60f
Amimul Ehsan <1805056@ugrad.cse.buet.ac.bd>
2021-10-21 23:01:57 +0600

    Deep diving into UserDetailsManager and PassWordEncoder

---
+++ spring-security-c3/src/main/java/com/rahi/springsecurityc3/SpringSecurityC3Application.java
@@ -0,0 +1,13 @@
+package com.rahi.springsecurityc3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityC3Application {
+
+    public static void main( String[] args ) {
+        SpringApplication.run(SpringSecurityC3Application.class, args);
+    }
+
+}
