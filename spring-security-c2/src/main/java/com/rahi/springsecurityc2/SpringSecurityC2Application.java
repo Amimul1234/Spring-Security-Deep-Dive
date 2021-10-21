10ace9ec60113e48745bfe92bb069228ced67d0c
Amimul Ehsan <1805056@ugrad.cse.buet.ac.bd>
2021-10-21 21:55:34 +0600

    Implementing spring user details

--- spring-security-c2/src/main/java/com/rahi/springsecurityc2/SpringSecurityC2Application.java
+++ spring-security-c2/src/main/java/com/rahi/springsecurityc2/SpringSecurityC2Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringSecurityC2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurityC2Application.class, args);
-	}
+    public static void main( String[] args ) {
+        SpringApplication.run(SpringSecurityC2Application.class, args);
+    }
 
 }
