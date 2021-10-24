40ec4d1d548d14ac386c2deaa9e8543b5f7c6e6a
Amimul Ehsan <1805056@ugrad.cse.buet.ac.bd>
2021-10-24 11:29:22 +0600

    Implementing 2-factor auth the correct way

---
+++ c6/src/main/java/com/rahi/c6/C6Application.java
@@ -0,0 +1,13 @@
+package com.rahi.c6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C6Application {
+
+    public static void main( String[] args ) {
+        SpringApplication.run(C6Application.class, args);
+    }
+
+}
---
+++ c6/src/test/java/com/rahi/c6/C6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahi.c6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
